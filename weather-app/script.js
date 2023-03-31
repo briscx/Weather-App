cbc9198721b48004440d6d1f7b83df7e18c3afa0
briscx <andrea.brischetto84@gmail.com>
2023-03-31 17:32:29 +0200

    git commit -m create html-page git commit -m create css-external-file git commit -m create js-external-file git commit -m add search-img git commit -m add weather-img

---
+++ weather-app/script.js
@@ -0,0 +1,63 @@
+let form = document.querySelector('form');
+
+let temperatureDescription = document.querySelector('.weather');
+let locationTimeZone = document.querySelector('.location-timezone');
+let temperatureDegree = document.querySelector('.temperature__section-degree');
+let temperatureSectionDegreeMin = document.querySelector('.temperature__section-degree-min');
+let temperatureSectionDegreeMax = document.querySelector('.temperature__section-degree-max');
+let temperatureUmidity = document.querySelector('.temperature__umidity');
+let input = document.querySelector('input');
+
+let lng;
+let lat;
+
+let locationIcon = document.querySelector('.location-icon');
+let lang = (navigator.language).substring(0, 2);
+
+const KELVIN_DEGREES = 273.15
+
+if (navigator.geolocation && input.value === '') {
+    console.log(input.value);
+    navigator.geolocation.getCurrentPosition((position) => {
+        lng = position.coords.longitude;
+        lat = position.coords.latitude;
+        const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=9f11284868fdf52b7ff9cdb0d1e61737&lang=${lang}`;
+        getData(api)
+    });
+}
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let city = input.value;
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=93d38192b6981451a1f4bd494daf32ba&lang=${lang}`;
+    getData(api)
+})
+
+
+function getData(url) {
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            setData(data)
+        });
+}
+
+
+function setData(data) {
+    const { name, weather, main } = data;
+
+    temperatureDegree.textContent = main.temp ? convertTemp(main.temp, 2) : 'not available'
+    temperatureDescription.textContent = weather[0]?.description.toUpperCase();
+    locationTimeZone.textContent = name ? name : 'city not available';
+    locationIcon.src = weather[0].icon ? `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png` : ''
+    temperatureSectionDegreeMin.textContent = main.temp_min ? convertTemp(main?.temp_min) : 'not available'
+    temperatureSectionDegreeMax.textContent = main.temp_max ? convertTemp(main.temp_max) : 'not available'
+    temperatureUmidity.textContent = `Umidità: ${main?.humidity}%`;
+}
+
+function convertTemp(temp, toFixed = 0) {
+    return parseFloat(temp - KELVIN_DEGREES).toFixed(toFixed);
+}
